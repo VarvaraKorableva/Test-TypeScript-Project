6c9d23ec88d75caefa33ab933d14236e61d8f21c
Varvara Korableva <v.korableva1990@yandex.ru>
2022-08-13 16:08:44 +0300

    added homepage in json, changed aboutPage

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,11 +2,7 @@ import React from "react"
 
 export function AboutPage() {
     return (
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Nulla exercitationem pariatur quis, quia porro beatae reprehenderit, 
-            eligendi veritatis magnam odio repellendus! 
-            Repellat, eaque. Ad laborum odit suscipit nulla incidunt temporibus dolore corporis quo dolorem rem optio laboriosam esse molestias nihil, 
-            rerum fugit quaerat voluptatem minima nisi! Iusto error animi suscipit!
+        <div>Thanks for visit this page
         </div>
     )
 }
